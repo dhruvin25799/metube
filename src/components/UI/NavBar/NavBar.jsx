390886ed703fe76df0144bbabba9cb001d795187
Dhruvin Mehta <dhruvin.mehta25799@gmail.com>
2022-04-18 21:15:05 +0530

    Updated Navbar

--- src/components/UI/NavBar/NavBar.jsx
+++ src/components/UI/NavBar/NavBar.jsx
@@ -1,10 +1,27 @@
 import styles from "./NavBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhotoFilm } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 export const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(()=>{
+    const handleScroll = () =>{
+      if(window.scrollY > 0){
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () =>{
+      window.removeEventListener("scroll", handleScroll);
+    }
+
+  },[]);
   return (
     <>
-      <nav className={styles["nav"]}>
+      <nav className={`${styles["nav"]} ${!isScrolled && styles["transparent"]}`}>
         <div className={styles["nav-brand"]}>
           <FontAwesomeIcon icon={faPhotoFilm} size="2x" />
           MeTube
